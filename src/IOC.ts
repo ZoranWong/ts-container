15268fa8c9094869b66b184b1c5bb78f6ca91f26
ZoranWong <904110231@qq.com>
2019-09-16 16:46:39 +0800

    添加IOC注释

--- src/IOC.ts
+++ src/IOC.ts
@@ -2,21 +2,43 @@ import Container from './Container';
 import "reflect-metadata";
 import IOCError from "./Expceptions/IOCError";
 
+/**
+* 构造函数接口
+* * */
+interface Ctor<T>{new (...args: Array<Function>): T}
+
+/**
+ * 容器对象
+ * @var
+ * @type {Container} container
+* */
 const container = new Container();
 
-function isReallyInstanceOf<T> (ctor: { new (...args: any[]): T }, obj: T) {
+/**
+ * instanceof 操作拓展
+* */
+function isReallyInstanceOf<T> (ctor: Ctor<T>, obj: T) {
     return obj instanceof ctor;
 }
 
+/**
+ * 构造函数接口拓展
+* */
 interface ConstructorInterface<T> {
     _name: string,
     _constructorStr: string,
-    _constructor: { new (...args: Array<any>): T },
+    _constructor: Ctor<T>,
     _paramTypes: Array<Function>
 }
 
+/**
+ * 注册前处理函数
+ * @param {T} target 注册对象
+ * @param {string} name 别名
+ * @return {ConstructorInterface<T>}
+* */
 function beforeInject<T> (target: any, name: string = null): ConstructorInterface<T> {
-    let _constructor: { new (...args: Array<Function>): T } = (target instanceof Function ? target : target.constructor);
+    let _constructor: Ctor<T> = (target instanceof Function ? target : target.constructor);
     let _constructorStr = _constructor.toString();
     let paramTypes: Array<Function> = Reflect.getMetadata('design:paramtypes', _constructor);
     if (name && container.bound(name)) {
@@ -33,6 +55,11 @@ function beforeInject<T> (target: any, name: string = null): ConstructorInterfac
     return {_name: name, _constructorStr: _constructorStr, _constructor: _constructor, _paramTypes: paramTypes};
 }
 
+/**
+ * 获取参数列表
+ * @param {Array<Function>} paramTypes 参数列表类型数组
+ * @return {Array<any>}
+ * */
 function getParamsInstances (paramTypes: Array<Function>): Array<any> {
     let paramInstances: Array<any> = paramTypes.map((v, i) => {
         // 参数不可注入
@@ -49,11 +76,22 @@ function getParamsInstances (paramTypes: Array<Function>): Array<any> {
     return paramInstances;
 }
 
-function createInstance<T> (paramTypes: Array<Function>, _constructor: { new (...args: Array<Function>): T }, args: Array<any>) {
+/**
+ * 创建实例
+ * @param {Array<Function>} paramTypes 参数类型数组
+ * @param {{new (...args: Array<Function>): T}}
+ * @return Function
+ * */
+function createInstance<T> (paramTypes: Array<Function>, _constructor: Ctor<T>, args: Array<any>) {
     let paramInstances: Array<any> = args.length > 0 ? args : getParamsInstances(paramTypes);
     return new _constructor(...paramInstances);
 }
 
+/**
+ * 将class（target）注册到容器中
+ * @param {String} name 别名默认没有
+ * @return Function
+ * */
 export function register (name: string = null): Function {
     return (target: any) => {
         let {_name, _constructorStr, _constructor, _paramTypes} = beforeInject(target, name);
@@ -66,6 +104,11 @@ export function register (name: string = null): Function {
     }
 }
 
+/**
+ * 为class（target）注册单例对象
+ * @param {String} name 别名默认没有
+ * @return Function
+ * */
 export function singleton (name: string = null): Function {
     return (target: any) => {
         let {_name, _constructorStr, _constructor, _paramTypes} = beforeInject(target, name);
@@ -78,6 +121,11 @@ export function singleton (name: string = null): Function {
     }
 }
 
+/**
+ * 工厂方法获取（创建）name对应的对象实例
+ * @param {String| T} name 类型或者别名
+ * @return T
+* */
 export function factory<T> (name: String | T): T {
     if (name instanceof String) {
         return container.get(name);

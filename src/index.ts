63db4c0eb714315840de233ab6b8911982e40ba7
ZoranWong <904110231@qq.com>
2019-09-16 17:22:15 +0800

    添加IOC类型代理

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import {isReallyInstanceOf, register, singleton, factory} from "./IOC";
 @singleton('IOC')
 class IOC {
     version: String = '0.0.1';
+
+    /**
+     * 静态实例类型注册函数
+     * @param {any} name 注册类型别名（或者注册类型 target = null时）
+     * @param {any} target 注册类型
+    * */
     public static register(name: any, target: any = null) {
         if(!target) {
             target = name;
@@ -13,6 +19,11 @@ class IOC {
         register(name)(target);
     }
 
+    /**
+     * 静态实例类型单例注册函数
+     * @param {any} name 注册类型别名（或者注册类型 target = null时）
+     * @param {any} target 注册类型
+     * */
     public static singleton (name: any, target: any = null) {
         if(!target) {
             target = name;
@@ -21,5 +32,19 @@ class IOC {
         singleton(name)(target);
     }
 }
-
-export default {isReallyInstanceOf, register, singleton, factory, IOC};
+/**
+ * IOC类型代理器
+* */
+let ioc = new Proxy(IOC, {
+    get (target: typeof IOC, p: PropertyKey, receiver: any): any {
+        switch (p) {
+            case 'singleton':
+                return target.singleton;
+            case 'register':
+                return target.register;
+            default:
+                return factory(p);
+        }
+    }
+});
+export default {isReallyInstanceOf, register, singleton, factory, ioc};

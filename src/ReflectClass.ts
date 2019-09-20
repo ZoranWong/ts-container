83863f548b42651268049e3cadb37fdaaf6e04bf
ZoranWong <904110231@qq.com>
2019-09-20 17:06:09 +0800

    ReflectClass继承ReflectFunction

--- src/ReflectClass.ts
+++ src/ReflectClass.ts
@@ -3,24 +3,14 @@ import ReflectionParameter from './ReflectionParameter';
 import IOCError from "./Expceptions/IOCError";
 import ReflectionFunction from "./ReflectionFunction";
 export default class ReflectionClass extends ReflectionFunction{
-    protected _class: any = null;
-
     protected _className: string = null;
 
     constructor($class: any) {
         super($class);
-        this._class = $class;
-        this._className = $class.name;
-        let params: Array<any> = ctorParamMetadata($class);
-        let paramTypes: Array<Function> = Reflect.getMetadata('design:paramtypes', $class);
+        this._className = this._callback.name;
+    }
 
-        params.forEach((value, index) => {
-            let paramType = paramTypes[index];
-            if(paramType === $class) {
-                throw  new IOCError('不可以依赖自身');
-            }
-            let parameter = new ReflectionParameter<typeof paramType>(paramType, value['value'], value['name']);
-            this._parameters.push(parameter);
-        });
+    protected  paramParse (): any[] {
+        return ctorParamMetadata(this._callback);
     }
 }
--- src/ReflectionFunction.ts
+++ src/ReflectionFunction.ts
@@ -1,5 +1,6 @@
-import { Closure } from './Utils/Types';
+import {Closure, ctorParamMetadata} from './Utils/Types';
 import ReflectionParameter from './ReflectionParameter';
+import IOCError from "./Expceptions/IOCError";
 export default class ReflectionFunction {
     protected _callback: Closure = null;
 
@@ -7,11 +8,26 @@ export default class ReflectionFunction {
 
     constructor(callback: Closure) {
         this._callback = callback;
+        let params: Array<any> = this.paramParse();
+        let paramTypes: Array<Function> = Reflect.getMetadata('design:paramtypes', this._callback);
+        params.forEach((value, index) => {
+            let paramType = paramTypes[index];
+            if(paramType === this._callback) {
+                throw  new IOCError('不可以依赖自身');
+            }
+            let parameter = new ReflectionParameter<typeof paramType>(paramType, value['value'], value['name']);
+            this._parameters.push(parameter);
+        });
     }
 
     public getParameters() {
         return this._parameters.map((param: ReflectionParameter<any>) => {
             return param.getValue();
         });
+    }
+
+    protected paramParse(): any[] {
+        console.log('----');
+        return [];
     }
 }
